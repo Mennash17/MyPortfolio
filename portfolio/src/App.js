948b1932bab2a5158d17abdd2348ef7f844f3972
Mennash17 <mennatallahsh17@gmial.com>
2023-10-27 21:55:49 +0200

    add react app

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './componants/project';
+import Project from './componants/project';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Project/>
+        <Project/>
+        <Project/>
+        <Project/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/componants/project.js
@@ -0,0 +1,13 @@
+
+const Project = () => {
+  return (
+    <div class="main_container">
+      <div class="project_name">Login Form</div>
+      <div class="project_tools">HTML , CSS</div>
+      <div class="project_info">FreeCodeCamp CSS Challenge</div>
+      <div class="project_link"><a href = 'https://mennash17.github.io/freecodecamp-form.io/'>Check it in here</a></div>
+    </div>
+  );
+};
+
+export default Project;
